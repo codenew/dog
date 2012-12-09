7260f68905e9e5e87cc7e56b005004532614ddd1
kaikai2 <zikaizhang@gmail.com>
2012-12-09 23:27:38 +0800

    force load pages before jquery.mobile, which ensure 'pageshow' events bind before jquery.mobile fires.

--- client/assets/www/js/app/pages/main.js
+++ client/assets/www/js/app/pages/main.js
@@ -1,35 +1,39 @@
 define(function(require, exports, module) {
-	var $ = require('jquery');
-	var DogServer = require('../api').DogServer;
-	var user = require('../models/user');
-	var preloadjs = require('preloadjs');
-	
-	function log(info){
-	    $('#log').prepend($('<p>').text(info));
-	}
-	$(document).delegate("#mainPage", "pageinit", function(){
-	    console.log('main page init');
-	    $("#mainPage #logout").click(function(){
-	        user.logout(function(){
-	            $.mobile.changePage('login.html');
-	        });
+    var $ = require('jquery');
+    var DogServer = require('../api').DogServer;
+    var user = require('../models/user');
+    //	var preloadjs = require('preloadjs');
+    
+    function log(info){
+	$('#log').prepend($('<p>').text(info));
+    }
+    $(document).delegate("#mainPage", "pageinit", function(){
+	console.log('main page init');
+	$("#mainPage #logout").click(function(){
+	    user.logout(function(){
+		$.mobile.changePage('login.html');
 	    });
-    }).delegate("#mainPage", "pageshow", function(){
-	    console.log('main page show');
-	    user.GetLocalUser(function(userinfo){
-	        if (null == userinfo){
-	            // auto switch to login page
-	            $.mobile.changePage('login.html');
-	            return;
-	        }
-	    });
-	    var canvas = document.getElementById('test');
-	    var context = canvas.getContext('2d');
-
-	    var bodyImg = new Image();
-	    bodyImg.src = '../../../img/pet0-arm.png';
-	    
-	    context.drawImage(bodyImg, 0,0, bodyImg.width, bodyImg.height);
 	});
+
+	
+    }).delegate("#mainPage", "pageshow", function(){
+	console.log('main page show');
+	user.GetLocalUser(function(userinfo){
+	    if (null == userinfo){
+		// auto switch to login page
+		$.mobile.changePage('login.html');
+		return;
+	    }
+	});
+	var canvas = document.getElementById('test');
+	var context = canvas.getContext('2d');
+	var bodyImg = new Image();	
+	
+	bodyImg.src = '../../../img/pet0-arm.png';
+
+	bodyImg.onload = function(){
+	    context.drawImage(bodyImg, 0,0, bodyImg.width, bodyImg.height);
+	}
+    });
 
 });
--- client/assets/www/js/index.js
+++ client/assets/www/js/index.js
@@ -1,95 +1,101 @@
 requirejs.config({
-	//By default load any module IDs from js/lib
-	baseUrl: 'js/lib',
-	
-	//except, if the module ID starts with "app",
-	//load it from the js/app directory. paths
-	//config is relative to the baseUrl, and
-	//never includes a ".js" extension since
-	//the paths config could be for a directory.
-	paths: {
-		app: '../app',
-		},
-	shim: {
-		underscore: {exports: '_'},
-		backbone: {deps:["underscore", "jquery"], exports: "Backbone"},
-		}
-	});
+    //By default load any module IDs from js/lib
+    baseUrl: 'js/lib',
+    
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+	app: '../app',
+    },
+    shim: {
+	underscore: {exports: '_'},
+	backbone: {deps:["underscore", "jquery"], exports: "Backbone"},
+	"jquery.mobile":{
+	    deps:[
+		"app/pages/main",
+		"app/pages/login",
+	    ],
+	}
+    }
+});
 requirejs(['underscore'], function(_){
-    });
+});
 define(function(require, exports, module) {
-	var $ = require('jquery')
-	    , jmobile = require('jquery.mobile')
-	    , _ = require('underscore')
-	    , backbone = require('backbone')
-	    , phonegap = require('phonegap')
-	    , login = require('app/pages/login')
-	    , main = require('app/pages/main')
-	    , user = require('app/models/user')
-	    , config = require('app/config');
-	
-	var host = config.host;
-	var port = config.port; 
-	function log(info){
-	    $('#log').prepend($('<p>').text(info));
+    var $ = require('jquery')
+     , _ = require('underscore')
+    , backbone = require('backbone')
+    , phonegap = require('phonegap')
+    , login = require('app/pages/login')
+    , main = require('app/pages/main')
+    , user = require('app/models/user')
+    , jmobile = require('jquery.mobile') 
+    , config = require('app/config');
+    
+    var host = config.host;
+    var port = config.port; 
+    function log(info){
+	$('#log').prepend($('<p>').text(info));
+    }
+    $(function(){
+	$('#add_log').click(function(){
+	    log('log add.');
+	});
+	//$('#test').click(function(){show();});
+	if ('undefined' != typeof WebSocketFactory){
+	    log('exist');
+	}else{
+	    log('no');
 	}
-	$(function(){
-		$('#add_log').click(function(){
-			log('log add.');
-		    });
-		//$('#test').click(function(){show();});
-		if ('undefined' != typeof WebSocketFactory){
-		    log('exist');
-		}else{
-		    log('no');
-		}
-	    });
-	// 等待加载PhoneGap
-	document.addEventListener("deviceready", onDeviceReady, false);
-	
-	// PhoneGap加载完毕，可以安全调用PhoneGap方法
-	function onDeviceReady() {
-	    log('device ready!');
-	    checkConnection(); 
-	    //	获取位置信息成功时调用的回调函数
-	    //	该方法接受一个“Position”对象，包含当前GPS坐标信息
-	    var onSuccess = function(position) {
-		log('Latitude: '          + position.coords.latitude          + '\n' +
-		    'Longitude: '         + position.coords.longitude         + '\n' +
-		    'Altitude: '          + position.coords.altitude          + '\n' +
-		    'Accuracy: '          + position.coords.accuracy          + '\n' +
-		    'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-		    'Heading: '           + position.coords.heading           + '\n' +
-		    'Speed: '             + position.coords.speed             + '\n' +
-		    'Timestamp: '         + new Date(position.timestamp)      + '\n');
-	    };
-	    
-	    // onError回调函数接收一个PositionError对象
-	    function onError(error) {
-		log('code: '    + error.code    + '\n' +
-		    'message: ' + error.message + '\n');
-	    }
-	    
-	    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-	}  
-	
-	function checkConnection() { 
-	    var networkState = navigator.network.connection.type; 		 
-	    
-	    var states = {}; 
-	    states[Connection.UNKNOWN]  = 'Unknown connection'; 
-	    states[Connection.ETHERNET] = 'Ethernet connection'; 
-	    states[Connection.WIFI]     = 'WiFi connection'; 
-	    states[Connection.CELL_2G]  = 'Cell 2G connection'; 
-	    states[Connection.CELL_3G]  = 'Cell 3G connection'; 
-	    states[Connection.CELL_4G]  = 'Cell 4G connection'; 
-	    states[Connection.NONE]     = 'No network connection'; 
-	    
-	    log('Connection type: ' + states[networkState]); 
-	}
-	
-	$("a[href]").click(function(){
-		$.mobile.changePage($(this).attr('href'));
-	    });
-	
     });
+    // 等待加载PhoneGap
+    document.addEventListener("deviceready", onDeviceReady, false);
+    
+    // PhoneGap加载完毕，可以安全调用PhoneGap方法
+    function onDeviceReady() {
+	log('device ready!');
+	checkConnection(); 
+	//	获取位置信息成功时调用的回调函数
+	//	该方法接受一个“Position”对象，包含当前GPS坐标信息
+	var onSuccess = function(position) {
+	    log('Latitude: '          + position.coords.latitude          + '\n' +
+		'Longitude: '         + position.coords.longitude         + '\n' +
+		'Altitude: '          + position.coords.altitude          + '\n' +
+		'Accuracy: '          + position.coords.accuracy          + '\n' +
+		'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+		'Heading: '           + position.coords.heading           + '\n' +
+		'Speed: '             + position.coords.speed             + '\n' +
+		'Timestamp: '         + new Date(position.timestamp)      + '\n');
+	};
+	
+	// onError回调函数接收一个PositionError对象
+	function onError(error) {
+	    log('code: '    + error.code    + '\n' +
+		'message: ' + error.message + '\n');
+	}
+	
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }  
+    
+    function checkConnection() { 
+	var networkState = navigator.network.connection.type; 		 
+	
+	var states = {}; 
+	states[Connection.UNKNOWN]  = 'Unknown connection'; 
+	states[Connection.ETHERNET] = 'Ethernet connection'; 
+	states[Connection.WIFI]     = 'WiFi connection'; 
+	states[Connection.CELL_2G]  = 'Cell 2G connection'; 
+	states[Connection.CELL_3G]  = 'Cell 3G connection'; 
+	states[Connection.CELL_4G]  = 'Cell 4G connection'; 
+	states[Connection.NONE]     = 'No network connection'; 
+	
+	log('Connection type: ' + states[networkState]); 
+    }
+    
+    $("a[href]").click(function(){
+	$.mobile.changePage($(this).attr('href'));
+    });
+    
+});
