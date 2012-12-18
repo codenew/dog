ed589c462817803d7373eb5b17d5b71521e495ab
kaikai2 <zikaizhang@gmail.com>
2012-12-18 22:06:15 +0800

    remove canvas over map, and correct the map position

--- client/assets/www/js/app/pages/position.js
+++ client/assets/www/js/app/pages/position.js
@@ -7,22 +7,31 @@ define(function(require, exports, module) {
         var myOptions = {
             zoom: 8,
             center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+	    panControl: true,
+	    zoomControl: true,
+	    zoomControlOptions: {
+		style: google.maps.ZoomControlStyle.DEFAULT,
+	    },
+	    mapTypeControl:false,
+	    scaleControl: true,
+	    streetViewControl: false,
+	    overviewMapControl: false,
         };
         var mapcontent = document.getElementById("mapcontent");
         var map = new google.maps.Map(mapcontent,
 				      myOptions);
-	    var canvas = document.getElementById('mapOverlay');
-		$(canvas).width($(mapcontent).width());
-		$(canvas).height($(mapcontent).height());
-		var context2d = canvas.getContext('2d');
-		context2d.fillStyle='#FF0000';
-		for (var i = 0; i < $(canvas).width(); i += 20){
-			context2d.fillRect(i,0,1,$(canvas).height());
-		}
-		for (var j = 0; j < $(canvas).height(); j += 20){
-			context2d.fillRect(0,j,$(canvas).width(),1);
-		}
+	/*var canvas = document.getElementById('mapOverlay');
+	$(canvas).width($(mapcontent).width());
+	$(canvas).height($(mapcontent).height());
+	var context2d = canvas.getContext('2d');
+	context2d.fillStyle='#FF0000';
+	for (var i = 0; i < $(canvas).width(); i += 20){
+	    context2d.fillRect(i,0,1,$(canvas).height());
+	}
+	for (var j = 0; j < $(canvas).height(); j += 20){
+	    context2d.fillRect(0,j,$(canvas).width(),1);
+	}*/
     }
     
     $(document).delegate("#mappage", "pageshow", initialize);
