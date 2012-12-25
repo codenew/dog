0593c0e9546dc956380a6d19c009a1459cc84f95
kaikai2 <zikaizhang@gmail.com>
2012-12-26 00:31:25 +0800

    circle model url with config.server path

--- client/assets/www/js/model/circle.js
+++ client/assets/www/js/model/circle.js
@@ -1,38 +1,43 @@
 define(function(require, exports, module){
-    var Backbone = require('backbone');
+    var Backbone = require('backbone')
+    , config = require('app/config');
     var local_circleManager = null;
     exports.Circle = Backbone.Model.extend({
-        urlBase:'/circle',
+        url:function(){
+	    return config.server + '/circle';
+	},
         idAttribute: 'id',
-	    location: null,
-	    radius: null,
-	    owner: 0,
+	location: null,
+	radius: null,
+	owner: 0,
     });
     
     exports.CircleManager = Backbone.Collection.extend({
-        url:'/circle',
-	    model: exports.Circle,
+        url:function(){
+	    return config.server + '/circle';
+	},
+	model: exports.Circle,
     });
-//    exports.setCircleManager = function(new_circleManager)
-//    {
-//        local_circleManager = new_circleManager;
-//    };
+    //    exports.setCircleManager = function(new_circleManager)
+    //    {
+    //        local_circleManager = new_circleManager;
+    //    };
     
-    exports.getCircleManager = function(next)
-    {
+    exports.getCircleManager = function(next){
         if (local_circleManager == null){ //Œ¥≥ı ºªØ
             local_circleManager = new this.CircleManager();
             local_circleManager.fetch({
                 success:function(collection, response, options){                                        
                     console.log("Fetch CirlesManager success!");                    
-                },
+		    next(local_circleManager);
+		},
                 error:function (collection, xhr, options){
                     console.log("Fetch CirlesManager failed!");
                     local_circleManager = null;
+		    next(null);
                 },
-             });       
-        }
-        else{
+            });
+        } else{
             next(local_circleManager);
         }
     };
