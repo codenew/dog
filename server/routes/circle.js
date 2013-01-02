d76f8db164ae8504d6c2db6f5927000aee376451
kaikai2 <zikaizhang@gmail.com>
2013-01-03 00:10:26 +0800

    check money while create new circle

--- server/routes/circle.js
+++ server/routes/circle.js
@@ -1,14 +1,12 @@
 var _ = require('underscore')
 , async = require("async")
+, mongodb = require('mongodb')
 , globaldata = require('../globaldata');
-var data = {
-    1: {id: 1, name: 'dsadas',   from: 1, to: 2, message: 'hi'},
-    2: {id: 2, name: 'dasdsada', from:1,to: 1, message: 'what'},
-    3: {id: 3, name: 'dasdsaf',  from:1, to: 2, message: 'just hi...'},
-};
-var lastid = 3;
+
+var ObjectID = mongodb.ObjectID;
+
 _.extend(exports, {
-    
+    // update
     put_one: function(id, req, res){
 	_.each(data, function(c){
 	    if (c.id == id){
@@ -17,6 +15,7 @@ _.extend(exports, {
 	});
 	res.json({result: 'ok'});
     },
+    // select
     get_all: function(req, res){
 	var location = req.param('location') || {latitude: 31, longitude:121};
 	location = {
@@ -51,6 +50,7 @@ _.extend(exports, {
         });        
 	console.log("get list");
     },
+    // select one
     get_one: function(id, req, res){
 	globaldata.get('mongoPool').acquire(req, 'circles', function(err, collection, release){
 	    if (err){
@@ -69,7 +69,8 @@ _.extend(exports, {
 		release();
 	    });
 	});
-    },    
+    },
+    // create one
     post_one: function(req, res){
 	var location = req.param('location') || {latitude: 31, longitude:121};
 	var newData = {
@@ -83,27 +84,77 @@ _.extend(exports, {
 	    loc: [location.longitude, location.latitude], // NOTE: mongo use [long,lat] pair
 	    radius: req.param('radius') || 10, // in meter
 	};
-	globaldata.get('mongoPool').acquire(req, 'circles', function(err, collection, release){
-	    if (err){
-		res.send(500, err);
-		return;
-	    }
-	    collection.insert(newData, function(err, r){
-		if (err){
-		    res.send(500, err);
-		    release();
-		    return;
-		}
-		console.log(r);
-		if (r.length != 1){
-		    res.send(500, 'length not 1');
-		    release();
-		    return;
-		}
-		res.json({id: r[0]._id});
-		release();
-	    });
-	});
+        var pool = globaldata.get('mongoPool');
+        var userid = req.session.userid;
+        async.waterfall([
+            function(next){
+                pool.acquire(req, 'users', function(err, collection, release){
+	            if (err){
+                        next(err);
+		        return;
+	            }
+                    var userObjectID = new ObjectID(userid);
+	            collection.findOne({_id: userObjectID}, function(err, user){
+		        if (err){
+                            next(err);
+		            release();
+		            return;
+		        }
+                        var createCircleCost = 10;
+                        if (user.money < createCircleCost){
+                            next('not enough money');
+		            release();
+                            return;
+                        }
+                        collection.update({_id: userObjectID}, {
+                            $inc: {money: -createCircleCost}
+                        }, function(err, modifyCount){
+                            if (err){
+                                console.log('createcircle.costmoney failed');
+                                next('cost money failed');
+                            }else{
+                                if (modifyCount == 1){
+                                    next(null);
+                                }else{
+                                    next('modify count not 1, which is:' + modifyCount);
+                                }
+                            }
+		            release();
+                        });
+	            });
+	        });
+            },
+            function(next){
+                pool.acquire(req, 'circles', function(err, collection, release){
+	            if (err){
+		        next(err);
+		        return;
+	            }
+	            collection.insert(newData, function(err, r){
+		        if (err){
+                            next(err);
+		            release();
+		            return;
+		        }
+		        console.log(r);
+		        if (r.length != 1){
+		            next('length not 1');
+		            release();
+		            return;
+		        }
+                        next({id: r[0]._id});
+		        release();
+	            });
+	        });
+            }
+        ], function(err, result){
+            if (err){
+                console.log(err);
+                res.send(500, err);
+            }else{
+		res.json(result);
+            }
+        });
     },
     delete_one: function(id, req, res){
 	delete data[id];
